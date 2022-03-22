941d31da34451782264169cd755630a8ce97f7c3
xiraynedev <xiraynedev@gmail.com>
2022-03-22 14:40:08 -0400

    fix button theme colors

--- src/components/ThemeButton/ThemeButton.tsx
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -10,7 +10,7 @@ export const ThemeButton: FC<ThemeButtonProps> = ({lightTheme, handleThemeClick}
     <button
       onClick={handleThemeClick}
       className={`${lightTheme ? 'bg-light-btn-bg text-light-btn-text-white' :
-        'bg-dark-btn-bg text-dark-btn-text'} p-2 rounded-lg text-white`}>{
+        'bg-dark-btn-bg text-dark-btn-text'} p-2 rounded-lg`}>{
       lightTheme ? 'Set Dark Theme' : 'Set Light Theme'
     }</button>
   );
